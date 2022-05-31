9df9588af2774c2d8f49ed6226809fe59772b5be
Angel <angel.villalobos2596@gmail.com>
2022-05-30 23:13:41 -0600

    Persistencia de datos
    Se guardan los datos del scrapping en mongoClodud

--- webscrapping.py
+++ webscrapping.py
@@ -1,30 +1,62 @@
-
-
-
-import requests 
+from datetime import date
+from distutils.log import info
+from urllib import request
+import requests
 from bs4 import BeautifulSoup
 
-page = requests.get("https://www.bmw.co.cr/es/index.html")
-soup = BeautifulSoup(page.content, 'html.parser')
 
-# print(soup.prettify())
-# print(soup.body)
+def get_database():
+    from pymongo import MongoClient
+    import pymongo
 
-#Links
-print('============== Links ==============')
-for link in soup.find_all('a'):
-    print(link.get('href'))
+    # Provide the mongodb atlas url to connect python to mongodb using pymongo
+    CONNECTION_STRING = "mongodb+srv://webscrapping-user:KbL7bDtumnkWyjCa@cluster0.lwh3p.mongodb.net/?retryWrites=true&w=majority"
+    # "mongodb+srv://<username>:<password>@cluster0.lwh3p.mongodb.net/?retryWrites=true&w=majority"
 
-#Textos
-print('============== Textos ==============')
-print(soup.get_text())
+    # Create a connection using MongoClient. You can import MongoClient or use pymongo.MongoClient
+    from pymongo import MongoClient
+    client = MongoClient(CONNECTION_STRING)
 
-#Listas
-print('============== Listas ==============')
-print(soup.ul)
+    # Create the database for our example (we will use the same database throughout the tutorial
+    return client['webscrapping']
 
-#Imagenes
-print('============== Imagenes ==============')
-# print(soup.img)
-for image in soup.find_all('img'):
-    print(image)
+
+# This is added so that many files can reuse the function get_database()
+if __name__ == "__main__":
+
+    # Get the database
+    webscrapping = get_database()
+
+    page = requests.get("https://books.toscrape.com/index.html")
+    soup = BeautifulSoup(page.content, 'html.parser')
+
+    li = soup.select("ol > li")
+    url = 'https://books.toscrape.com/'
+
+    # auditoria
+    auditoría = {
+        "fecha": str(date.today()),
+        "pagina_web": "https://books.toscrape.com/index.html",
+        "numero_registros": len(li),
+        "estado": "finalizado",
+        "errores": "none"
+    }
+    # inserta en la db
+    webscrapping.auditoria.insert_one(auditoría)
+
+    # print(len(li))
+    for link in li:
+        page = requests.get(url + link.find('a').get('href'))
+        sopa = BeautifulSoup(page.content, 'html.parser')
+        titulo = sopa.find('h1').get_text()
+        BOOK = {'titulo': titulo}
+        tabla = sopa.find("table")
+        tr = tabla.find_all('tr')
+        for t in tr:
+            prod_info = {
+                **BOOK, t.find('th').get_text(): t.find('td').get_text()}
+            BOOK = {**prod_info}
+        # print(BOOK)
+
+        # insertar en la db
+        webscrapping.registros.insert_one(BOOK)
